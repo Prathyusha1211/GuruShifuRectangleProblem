e560ddf8bbec8e2d6405403a09b6dd4ce6debea7
Prathyusha1211 Melam <n171211@rguktn.ac.in>
2024-05-31 11:20:10 +0000

    Refactor. Added some more test cases to the RectangleTest class

--- src/main/java/com/thoughtworks/rectangle/Rectangle.java
+++ src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -3,6 +3,9 @@ package com.thoughtworks.rectangle;
 public class Rectangle{
     int getAreaOfRectangle(int length, int breadth)
     {
+        if(length<0 || breadth<0){
+            throw new IllegalArgumentException();
+        }
         return length*breadth;
     }
 }
--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -1,16 +1,43 @@
 package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Tests for Area of Rectangle")
 public class RectangleTest {
-    @Test
-    void shouldReturnAreaOfRectangle(){
-        Rectangle rectangleObj = new Rectangle();
-        int ActualArea = rectangleObj.getAreaOfRectangle(3,4);
-        int ExpectedArea = 12;
-        assertThat("When Length and Breadth are given currect", ExpectedArea,is(ActualArea));
+
+    Rectangle obj = new Rectangle();
+
+    @Nested
+    @DisplayName("When Length are Breadth are Integers")
+    class InputsAreIntegers{
+        @Test
+        @DisplayName("Actual and Expected Area of Rectangle Matches")
+        public void shouldReturnAreaOfRectangleWhenLengthAndBreadthAreGivenCurrect(){
+            int ExpectedArea = 12;
+            int ActualArea = obj.getAreaOfRectangle(3,4);
+            assertThat(ActualArea,is(ExpectedArea));
+        }
+
+        @Test
+        @DisplayName("Actual and Expected Area of Rectangle doesn't Matches")
+        public void shouldReturnWrongOutputWhenLengthAndBreadthAreGivenWrong(){
+            int ExpectedArea = 14;
+            int ActualArea = obj.getAreaOfRectangle(3,5);
+            assertThat(ActualArea,is(not(ExpectedArea)));
+        }
+
+        @Test()
+        @DisplayName("Throws error when any input parameter is negative")
+        public void shouldReturnExceptionWhenLengthOrBreadthGivenNegative(){
+            assertThrows(IllegalArgumentException.class,()->obj.getAreaOfRectangle(-3,9));
+        }
+
     }
+    
 }
